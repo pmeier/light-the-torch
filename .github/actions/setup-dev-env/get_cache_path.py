04fbc92f22fe0417d5dbd1390e115775d1dfc955
Philip Meier <github.pmeier@posteo.de>
2022-03-02 23:18:10 +0100

    setup cache

--- .github/actions/setup-dev-env/get_cache_path.py
+++ .github/actions/setup-dev-env/get_cache_path.py
@@ -1,8 +1,14 @@
 import os
+import pathlib
 
 
 def main():
-    print(os.environ["OS"])
+    rel_path = {
+        "Linux": pathlib.PurePosixPath(".cache") / "pip",
+        "Windows": pathlib.PureWindowsPath("AppData") / "Local" / "pip" / "Cache",
+        "macOS": pathlib.PurePosixPath("Library") / "Caches" / "pip",
+    }[os.environ["OS"]]
+    print(pathlib.Path.home() / rel_path)
 
 
 if __name__ == "__main__":
