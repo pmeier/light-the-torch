7308bffee12a982b7f5f6e106beb25c116540512
Philip Meier <github.pmeier@posteo.de>
2022-03-18 21:38:37 +0100

    refactor dodo (#47)
    * refactor dodo
    
    * fix test CI
    
    * address review comments
    
    Co-authored-by: Tony Fast <tony.fast@gmail.com>
    
    Co-authored-by: Tony Fast <tony.fast@gmail.com>

--- dodo.py
+++ dodo.py
@@ -1,107 +1,150 @@
-import functools
+import os
 import pathlib
+import shlex
 
-from doit.action import CmdAction as _CmdAction
+from doit.action import CmdAction
 
-CmdAction = functools.partial(_CmdAction, shell=False)
 
 HERE = pathlib.Path(__file__).parent
+PACKAGE_NAME = "light_the_torch"
+
+CI = os.environ.get("CI") == "1"
+
+DOIT_CONFIG = dict(
+    verbosity=2,
+    backend="json",
+)
 
 
-def task_setup(pip="pip"):
-    return dict(
+def do(*cmd):
+    if len(cmd) == 1:
+        cmd = cmd[0]
+    if isinstance(cmd, str):
+        cmd = shlex.split(cmd)
+    return CmdAction(cmd, shell=False, cwd=HERE)
+
+
+def _install_dev_requirements(pip="python -m pip"):
+    return f"{pip} install -r requirements-dev.txt"
+
+
+def _install_project(pip="python -m pip"):
+    return f"{pip} install -e ."
+
+
+def task_install():
+    """Installs all development requirements and light-the-torch in development mode"""
+    yield dict(
+        name="dev",
+        file_dep=[HERE / "requirements-dev.txt"],
+        actions=[do(_install_dev_requirements())],
+    )
+    yield dict(
+        name="project",
         actions=[
-            [pip, "install", "-r", HERE / "requirements-dev.txt"],
-            [pip, "install", "-e", HERE],
+            do(_install_project()),
         ],
     )
 
 
-def task_dev_env():
+def task_setup():
+    """Sets up a development environment for light-the-torch"""
     dev_env = HERE / ".venv"
+    pip = dev_env / "bin" / "pip"
     return dict(
         actions=[
-            ["rm", "-rf", dev_env],
-            ["virtualenv", dev_env, "--prompt=(light-the-torch-dev) "],
-            *task_setup(dev_env / "bin" / "pip")["actions"],
-            [
-                "echo",
-                f"run `source {dev_env / 'bin' / 'activate'}` the virtual environment",
-            ],
+            do(f"virtualenv {dev_env} --prompt='(light-the-torch-dev) '"),
+            do(_install_dev_requirements(pip)),
+            do(_install_project(pip)),
+            lambda: print(
+                f"run `source {dev_env / 'bin' / 'activate'}` the virtual environment"
+            ),
         ],
-        verbosity=2,
+        clean=[do(f"rm -rf {dev_env}")],
+        uptodate=[lambda: dev_env.exists()],
     )
 
 
 def task_format():
-    return dict(actions=[["pre-commit", "run", "--all-files"]])
+    """Auto-formats all project files"""
+    return dict(
+        actions=[
+            do("pre-commit run --all-files"),
+        ]
+    )
 
 
 def task_lint():
+    """Lints all project files"""
     return dict(
-        actions=[["flake8", "--config=.flake8"]],
+        actions=[
+            do("flake8 --config=.flake8"),
+        ]
     )
 
 
 def task_test():
-    def run(coverage, passthrough):
-        cmd = [
+    """Runs the test suite"""
+
+    def run(passthrough):
+        return [
             "pytest",
             "-c",
             HERE / "pytest.ini",
+            f"--cov-report={'xml' if CI else 'term'}",
+            *passthrough,
         ]
-        if coverage:
-            cmd.extend(
-                [
-                    f"--cov={HERE / 'light_the_torch'}",
-                    "--cov-report=xml",
-                    f"--cov-config={HERE / '.coveragerc'}",
-                ]
-            )
-        cmd.extend(passthrough)
-        return cmd
 
     return dict(
-        actions=[CmdAction(run)],
-        verbosity=2,
-        params=[
-            dict(
-                name="coverage",
-                long="coverage",
-                type=bool,
-                default=False,
-            )
-        ],
+        actions=[do(run)],
         pos_arg="passthrough",
     )
 
 
-def task_publishable():
+def task_build():
+    """Builds the source distribution and wheel of light-the-torch"""
     return dict(
         actions=[
-            [
-                "rm",
-                "-rf",
-                HERE / "build",
-                HERE / "dist",
-                HERE / "light_the_torch.egg-info",
-            ],
-            ["python", "-m", "build", "--sdist", "--wheel", HERE],
-            # We need the lambda here to lazily glob the files in dist/*, since they
-            # are only created by the previous step rather than when this task is
-            # created.
-            CmdAction(lambda: ["twine", "check", *list((HERE / "dist").glob("*"))]),
-            ["check-wheel-contents", HERE / "dist"],
+            do("python -m build ."),
+        ],
+        clean=[
+            do(f"rm -rf build dist {PACKAGE_NAME}.egg-info"),
         ],
     )
 
 
-def task_publish():
-    task = task_publishable()
-    # We need the lambda here to lazily glob the files in dist/*, since they are only
-    # created by the previous step rather than when this task is created.
-    task["actions"].append(
-        CmdAction(lambda: ["twine", "upload", *list((HERE / "dist").glob("*"))])
+def task_publishable():
+    """Checks if metadata is correct"""
+    yield dict(
+        name="twine",
+        actions=[
+            # We need the lambda here to lazily glob the files in dist/*, since they
+            # are only created by the build task rather than when this task is
+            # created.
+            do(lambda: ["twine", "check", *list((HERE / "dist").glob("*"))]),
+        ],
+        task_dep=["build"],
     )
-    task["verbosity"] = 2
-    return task
+    yield dict(
+        name="check-wheel-contents",
+        actions=[
+            do("check-wheel-contents dist"),
+        ],
+        task_dep=["build"],
+    )
+
+
+def task_publish():
+    """Publishes light-the-torch to PyPI"""
+    return dict(
+        # We need the lambda here to lazily glob the files in dist/*, since they are
+        # only created by the build task rather than when this task is created.
+        actions=[
+            do(lambda: ["twine", "upload", *list((HERE / "dist").glob("*"))]),
+        ],
+        task_dep=[
+            "lint",
+            "test",
+            "publishable",
+        ],
+    )
