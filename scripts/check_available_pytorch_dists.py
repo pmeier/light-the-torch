4908d4b01269b2d426be5d78c4108915d64bf0c5
Philip Meier <github.pmeier@posteo.de>
2022-05-13 09:01:18 +0200

    remove comments

--- scripts/check_available_pytorch_dists.py
+++ scripts/check_available_pytorch_dists.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import itertools
-import json
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,10 +14,6 @@ EXCLUDED_PYTORCH_DIST = {
     "torch_cuda80",
     "torch_nightly",
     "torchaudio_nightly",
-    # "torchrec_nightly",
-    # "torchrec_nightly_3.7_cu11.whl",
-    # "torchrec_nightly_3.8_cu11.whl",
-    # "torchrec_nightly_3.9_cu11.whl",
 }
 PATCHED_PYTORCH_DISTS = set(PYTORCH_DISTRIBUTIONS)
 
